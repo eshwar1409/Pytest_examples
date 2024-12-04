d983446d39a8be9ec9ed13f61d0b0a43d161d83a
eshwar1409 <126580671+eshwar1409@users.noreply.github.com>
2024-12-04 17:06:00 +0530

    Add files via upload

---
+++ test_demo1.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+class TestMathOperations:
+    def test_addition(self):
+        assert 1 + 2 == 3
+
+    def test_subtraction(self):
+        assert 4 - 2 == 2
+
+
+    def test_multiplication(self):
+        assert 3 * 4 == 12
+
+    def test_division(self):
+        assert 8 / 2 == 4
+
+if __name__ == "__main__":
+  pytest.main()
+
+
+
+
