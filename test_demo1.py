aaa920cfd3316f2f3ffed9bbdcb9c3cfac6fab9b
eshwar1409 <126580671+eshwar1409@users.noreply.github.com>
2024-12-04 18:08:33 +0530

    Update test_demo1.py

--- test_demo1.py
+++ test_demo1.py
@@ -2,17 +2,17 @@ import pytest
 
 
 class TestMathOperations:
-    def test_addition(self):
+    def test_addition(T1):
         assert 1 + 2 == 3
 
-    def test_subtraction(self):
+    def test_subtraction(T2):
         assert 4 - 2 == 2
 
 
-    def test_multiplication(self):
+    def test_multiplication(T3):
         assert 3 * 4 == 12
 
-    def test_division(self):
+    def test_division(T4):
         assert 8 / 2 == 4
 
 if __name__ == "__main__":
